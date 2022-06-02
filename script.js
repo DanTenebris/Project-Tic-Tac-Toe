b6f5afcd0b744faf3ba330047c6f9df4b02ebc7d
DanTenebris <dantenebris@outlook.com>
2022-06-02 17:20:20 -0400

    Basic user interface completed

--- script.js
+++ script.js
@@ -9,10 +9,6 @@ const Player = (name) => {
 const player1 = Player('Player 1');
 const player2 = Player('Player 2');
 
-//player1.setName('George');
-console.log(player1.getName());
-console.log(player2.getName());
-
 const gameBoard = (() => {
   const _marks = [];
 
@@ -24,7 +20,7 @@ const gameBoard = (() => {
   }
 
   const getPlayerFlag = () => _playerFlag;
-  //const setPlayerFlag = (flag) => _playerFlag = flag;
+  const setPlayerFlag = (flag) => _playerFlag = flag;
 
   const _checkIfAllTrue = (value) => {
     return (value);
@@ -41,64 +37,159 @@ const gameBoard = (() => {
   //012 345 678
   //036 147 258
   //048 246
-  const _checkIfFinished = (player) => {
-
-    const _finishGame = (message = true) => {
-      if (message) {
-        displayController.showWinnerMessage(player);
-      } else {
-        displayController.showWinnerMessage();
-      }
-
-      console.log(player);
-      _removeOnClick(_markBoxes);
-      removeAllMarks()
-    };
+  const _evaluate = () => {
 
     if ((_marks[0]) &&
       ((_marks[0] === _marks[1] && _marks[1] === _marks[2]) ||
         (_marks[0] === _marks[3] && _marks[3] === _marks[6]) ||
         (_marks[0] === _marks[4] && _marks[4] === _marks[8]))) {
-      _finishGame();
+      if (_marks[0] === 'o') {
+        return -10;
+      } else {
+        return 10;
+      }
     }
     else if ((_marks[3]) &&
       (_marks[3] === _marks[4] && _marks[4] === _marks[5])) {
-      _finishGame();
+      if (_marks[3] === 'o') {
+        return -10;
+      } else {
+        return 10;
+      }
     }
     else if ((_marks[6]) &&
       (_marks[6] === _marks[7] && _marks[7] === _marks[8])) {
-      _finishGame();
+      if (_marks[6] === 'o') {
+        return -10;
+      } else {
+        return 10;
+      }
     }
     else if ((_marks[1]) &&
       (_marks[1] === _marks[4] && _marks[4] === _marks[7])) {
-      _finishGame();
+      if (_marks[1] === 'o') {
+        return -10;
+      } else {
+        return 10;
+      }
     }
     else if ((_marks[2]) &&
       ((_marks[2] === _marks[5] && _marks[5] === _marks[8]) ||
         (_marks[2] === _marks[4] && _marks[4] === _marks[6]))) {
-      _finishGame();
+      if (_marks[2] === 'o') {
+        return -10;
+      } else {
+        return 10;
+      }
     } else if (_marks.every(_checkIfAllTrue)) {
-      _finishGame(false);
+      return 0;
+    }else{
+      return false;
     }
   };
 
   const _togglePlayerFlag = () => {
     _playerFlag = _playerFlag ? false : true;
-    return _playerFlag;
+  };
+
+  const _finishGame = (message = true) => {
+    if (message) {
+      displayController.showWinnerMessage(getPlayerFlag() ? player1.getName() : player2.getName());
+    } else {
+      displayController.showWinnerMessage();
+    }
+
+    console.log(getPlayerFlag() ? player1.getName() : player2.getName());
+    _removeOnClick(_markBoxes);
+    removeAllMarks();
+  };
+
+  const _checkIfFinished = () => {
+    const _evaResult = _evaluate();
+    if ((_evaResult === -10) || (_evaResult === 10)) {
+      _finishGame();
+    } else if (_evaResult === 0) {
+      _finishGame(false);
+    }
+
   };
 
   const add = (index) => {
     _marks.splice(index, 1, getPlayerFlag() ? 'o' : 'x');
-    _checkIfFinished(getPlayerFlag() ? player1.getName() : player2.getName());
 
+    _checkIfFinished();
     _togglePlayerFlag();
-    console.log(_marks);
   };
 
-  return { getPlayerFlag, add, removeAllMarks };
+  return { getPlayerFlag, setPlayerFlag, add, removeAllMarks };
 })();
 
 
+const iaController = (() => {
+  const _markBoxes = Array.from(document.querySelectorAll('.mark-box'));
+
+  const _playAgainButton = document.querySelector('.gameOver-div > button:first-of-type');
+
+  let _iaOn = false;
+
+  const turnOn = () => _iaOn = true;
+  const turnOff = () => _iaOn = false;
+
+  const getIaState = () => _iaOn;
+
+  const _selectRandomElement = () => {
+    const _emptyBoxes = _markBoxes.filter(ele => !ele.hasChildNodes());
+
+    if (_emptyBoxes.length) {
+      const _box = _emptyBoxes[Math.floor(Math.random() * _emptyBoxes.length)];
+      return _box;
+    } else {
+      return false;
+    }
+  };
+
+  const _clickRandomBox = () => {
+    const _selectedElement = _selectRandomElement();
+    if ((_selectedElement) && (!gameBoard.getPlayerFlag())) _selectedElement.click();
+  };
+
+  const _clickTimer = () => setTimeout(_clickRandomBox, 1000);
+
+  const _clickWithPlayerFlag = () => {
+    if (!gameBoard.getPlayerFlag()) _clickTimer();
+  };
+
+  const clicking = () => {
+    for (const _markBox of _markBoxes) {
+      if (_markBox.getAttribute('listenerOnClick') !== 'true') {
+
+        _markBox.addEventListener('click', _clickWithPlayerFlag);
+
+        _markBox.setAttribute('listenerOnClick', 'true');
+      }
+    }
+    if (_iaOn) {
+      if (_playAgainButton.getAttribute('listenerOnClick') !== 'true') {
+        _playAgainButton.addEventListener('click', _clickWithPlayerFlag);
+        _playAgainButton.setAttribute('listenerOnClick', 'true');
+      }
+    }
+  }
+
+  const removeMarkListener = () => {
+    for (const _markBox of _markBoxes) {
+      _markBox.removeEventListener('click', _clickWithPlayerFlag);
+      _markBox.setAttribute('listenerOnClick', 'false');
+    }
+  };
+
+  const removePlayAgainListener = () => {
+    _playAgainButton.removeEventListener('click', _clickWithPlayerFlag);
+    _playAgainButton.setAttribute('listenerOnClick', 'false');
+  };
+
+  return { turnOn, turnOff, getIaState, clicking, removeMarkListener, removePlayAgainListener };
+})();
 
 
 const displayController = (() => {
@@ -110,13 +201,42 @@ const displayController = (() => {
   const _cancelButton = document.querySelector('.cancel');
   const _submitButton = document.querySelector('.submit');
 
+  const _oponentDiv = document.querySelector('.oponent-div');
+  const _vsUserBtn = document.querySelector('.vs-user');
+  const _vsAiBtn = document.querySelector('.vs-ai');
+
+  const _playerOneTurn = document.querySelector('.p-one');
+  const _playerTwoTurn = document.querySelector('.p-two');
+
   const _markBoxes = Array.from(document.querySelectorAll('.mark-box'));
 
-  
   const _winnerMessage = document.querySelector('.winner-message');
   const _playAgainButton = document.querySelector('.gameOver-div > button:first-of-type');
   const _menuBtn = document.querySelector('.gameOver-div > button:last-of-type');
 
+
+  const _toggleTurnColor = (flag = 'remove') => {
+    if (flag === 'remove') {
+      _playerOneTurn.style.backgroundColor = '#fce7f3';
+      _playerOneTurn.style.color = '#000000';
+      _playerTwoTurn.style.backgroundColor = '#fce7f3';
+      _playerTwoTurn.style.color = '#000000';
+    }
+    else if (flag) {
+      _playerOneTurn.style.backgroundColor = '#be185d';
+      _playerOneTurn.style.color = '#ffffff';
+
+      _playerTwoTurn.style.backgroundColor = '#fce7f3';
+      _playerTwoTurn.style.color = '#000000';
+
+    } else {
+      _playerTwoTurn.style.backgroundColor = '#be185d';
+      _playerTwoTurn.style.color = '#ffffff';
+
+      _playerOneTurn.style.backgroundColor = '#fce7f3';
+      _playerOneTurn.style.color = '#000000';
+    }
+  };
 
   const _addingMark = () => {
     for (const markBox of _markBoxes) {
@@ -129,6 +249,7 @@ const displayController = (() => {
             oMark.className = 'mark';
             oMark.src = 'media/o.svg';
             markBox.appendChild(oMark);
+
           } else {
             const xMark = document.createElement('img');
             xMark.className = 'mark';
@@ -136,6 +257,7 @@ const displayController = (() => {
             markBox.appendChild(xMark);
           }
 
+          _toggleTurnColor(!gameBoard.getPlayerFlag());
           gameBoard.add(_markBoxes.indexOf(markBox));
         }
       }
@@ -143,9 +265,9 @@ const displayController = (() => {
   };
 
   const _removeMarks = () => {
-    for (const markBox of _markBoxes) {
-      while (markBox.firstChild) {
-        markBox.removeChild(markBox.firstChild);
+    for (const _markBox of _markBoxes) {
+      while (_markBox.firstChild) {
+        _markBox.removeChild(_markBox.firstChild);
       }
     }
   };
@@ -180,27 +302,50 @@ const displayController = (() => {
       _toggleElementGridNone(_mainGame);
       _toggleElementGridNone(_initialDiv);
       _removeMarks();
+      gameBoard.setPlayerFlag(true);
       gameBoard.removeAllMarks();
+      if (iaController.getIaState()) {
+        iaController.turnOff();
+        iaController.removeMarkListener();
+        iaController.removePlayAgainListener();
+      }
+    };
+  };
+
+  const _startGame = () => {
+    _toggleElementGridNone(_oponentDiv);
+    _toggleElementGridNone(_mainGame);
+    _toggleElementBlockNone(_menuBtn);
+    _toggleMenuBtnPosition(true);
+    _addingMark();
+    _showMenu();
+    _toggleTurnColor(gameBoard.getPlayerFlag());
+  };
+
+  const _oponentBtns = () => {
+    _vsUserBtn.onclick = () => {
+      _startGame();
+    };
+
+    _vsAiBtn.onclick = () => {
+      _startGame();
+      iaController.turnOn();
+      iaController.clicking();
+      console.log('AI');
     };
   };
 
   const _playButton = () => {
     document.querySelector('.play-button').onclick = () => {
       _toggleElementGridNone(_initialDiv);
-      _toggleElementGridNone(_mainGame);
-      _toggleElementBlockNone(_menuBtn);
-      _toggleMenuBtnPosition(true);
-      _addingMark();
-      _showMenu();
+      _toggleElementGridNone(_oponentDiv);
+      _oponentBtns();
     }
   };
 
   const _writeError = (inputEl, errorElement) => {
     if (!inputEl.validity.valid) {
-      if (inputEl.validity.valueMissing) {
-        console.log('value missing');
-      }
-      else if (inputEl.validity.tooLong) {
+      if (inputEl.validity.tooLong) {
         errorElement.textContent = `The name must be shorter than ${inputEl.maxLength}; you entered ${inputEl.value.length}`;
       }
       errorElement.className = 'error active';
@@ -216,7 +361,7 @@ const displayController = (() => {
   const initialButtonListeners = () => {
     _playButton();
     _addNameButton.onclick = () => {
-      
+
       _toggleElementGridNone(_formDiv);
 
 
@@ -231,21 +376,22 @@ const displayController = (() => {
       if (!_submitButton.hasAttribute('listenerOnClick')) {
         _submitButton.addEventListener('click', () => {
 
-          if (_checkValidityAndLength(_inputElements[0])) {
-            console.log('h');
-            player1.setName(_inputElements[0].value);
-          } else if (_checkValidityAndLength(_inputElements[1])) {
-            player2.setName(_inputElements[1].value);
-          } else {
-            for (const inputElement of _inputElements) {
-              if (!inputElement.validity.valid) {
-                _writeError(inputElement, inputElement.nextElementSibling);
-                console.log('164');
-                return;
-              }
+          for (const inputElement of _inputElements) {
+            if (!inputElement.validity.valid) {
+              _writeError(inputElement, inputElement.nextElementSibling);
+              return;
             }
           }
 
+          if (_checkValidityAndLength(_inputElements[0])) {
+            player1.setName(_inputElements[0].value);
+            _playerOneTurn.textContent = player1.getName();
+          }
+
+          if (_checkValidityAndLength(_inputElements[1])) {
+            player2.setName(_inputElements[1].value);
+            _playerTwoTurn.textContent = player2.getName();
+          }
 
           _toggleElementGridNone(_formDiv);
         });
@@ -263,6 +409,10 @@ const displayController = (() => {
       _toggleMenuBtnPosition();
       _removeMarks();
       _addingMark();
+      _toggleTurnColor(gameBoard.getPlayerFlag());
+      if (iaController.getIaState()) {
+        iaController.clicking();
+      }
     };
   };
 
@@ -276,6 +426,10 @@ const displayController = (() => {
     _toggleElementBlockNone(_playAgainButton);
     _toggleMenuBtnPosition();
     _playAgain();
+    _toggleTurnColor('remove');
+    if (iaController.getIaState()) {
+      iaController.removeMarkListener();
+    }
   };
 
   return { initialButtonListeners, showWinnerMessage };
